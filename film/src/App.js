8c9de3729ffdc35cb36667a20c284193122787af
DESKTOP-KLDMOUR\Admin <kajetanwujke2004@gmail.com>
2024-09-27 10:47:02 +0200

    pole edycyjne i jego etykieta o treści „Tytuł filmu”

--- film/src/App.js
+++ film/src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <label for="film" class="form-label">tytuł filmu</label>
+        <input type="text" class="form-control" id="film" />
       </div>
     </div>
   );
